f30637d70b9765031cc6a5240a6656d9e9ac6f66
xiaowuzhi <1015331383@qq.com>
2018-07-02 07:45:15 +0800

    开发

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,26 +3,48 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostController extends Controller {
     //列表页面
     public function index() {
-        return view("post/index", array());
+        $posts = Post::orderBy('created_at', 'desc')->paginate(10);
+        //dd($posts);
+        return view("post/index", compact('posts'));
     }
 
     //详情页
-    public function show() {
-        return view("post/show", ['title' => 'this is title', 'isshow' => false]);
+    public function show(Post $post) {
+        return view("post/show", compact('post'));
     }
 
     //创建文章
     public function create() {
-        return view("post/create", array());
+        //dd(csrf_token());
+        return view("post/create");
     }
 
     //创建逻辑
     public function store() {
-        return;
+        //dd(\Request::all()); request();
+//        $post = new Post();
+//        $post->title = request('title');
+//        $post->content = request('content');
+//        $post->save();
+        //$params = ['title' => request('title'), 'content' => request('content')];
+        //验证
+        $this->validate(request(), [
+            'title' => 'required|string|max:100|min:5',
+            'content' => 'required|string|min:10',
+        ], [
+            //'title.min' => '文章标题过短'
+        ]);
+        //逻辑
+        $params = request(['title', 'content']);
+        $post = Post::create($params);
+
+        //渲染
+        return redirect('/posts');
 
     }
 
@@ -42,5 +64,16 @@ class PostController extends Controller {
     public function delete() {
         return;
 
+    }
+
+    //上传图片
+    public function imageUpload(Request $request) {
+
+        //print_r(request()->all());exit;
+        //dd(request()->all());
+        $path = $request->file('vvawangEditorH5File')->storePublicly(md5(time()));
+
+        $vva_class = (object)['errno'=>0, 'data'=>[asset('storage/' . $path)]];
+        return json_encode($vva_class);
     }
 }
