d020b05e0819186f16e00343ebe5762db323e28a
xiaowuzhi <1015331383@qq.com>
2018-07-03 17:45:30 +0800

    第一次使用 phpstorm git vcs

--- app/User.php
+++ app/User.php
@@ -2,28 +2,14 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
+//use Illuminate\Database\Eloquent\Model;
+use App\Model;
+
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
-{
-    use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
+class User extends Authenticatable {
+    public $fillable = [
+        'name', 'email', 'password'
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,22 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+//用户模块
+//注册页面
+Route::get('/register', '\App\Http\Controllers\RegisterController@index');
+//注册行为
+Route::post('/register', '\App\Http\Controllers\RegisterController@register');
+//登录页面
+Route::get('/login', '\App\Http\Controllers\LoginController@index');
+//登录行为
+Route::post('/login', '\App\Http\Controllers\LoginController@login');
+//登出行为
+Route::get('/logout', '\App\Http\Controllers\LoginController@logout');
+//个人设置页面
+Route::get('/user/me/setting', '\App\Http\Controllers\UserController@setting');
+//个人设置操作
+Route::post('/user/me/setting', '\App\Http\Controllers\UserController@settingStore');
+
 
 Route::group([], function(){
     // 文章
